af055a7df8c452e1e1e837f44316b7e234186012
Rolles <peronsos@gmail.com>
2022-05-05 04:32:34 +0300

    Initial commit

---
+++ src/main/java/com/veniro/VeniroApplication.java
@@ -0,0 +1,13 @@
+package com.veniro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeniroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VeniroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/veniro/VeniroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veniro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeniroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
